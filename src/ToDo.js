dce4e402f2ab112e8336772f6629e63cd5f952f0
Guilherme Kuhn <98616251+GDKuhn@users.noreply.github.com>
2022-10-26 14:49:27 -0300

    separa

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
 import "./ToDo.css";
+import List from "./List";
+import Form from "./Form";
 
 function ToDo() {
-    const [textToDo, setText] = useState("");
+    const [items, setItemsList] = useState([]);
 
-    function handleChange(event) {
-        let textInput = event.target.value;
-        setText(textInput);
+    function onAddItemToDo(text) {
+        setItemsList([...items, text]);
     }
+
     return (
         <div className="container">
             <h1>ToDo</h1>
-            <form>
-                <input onChange={handleChange} type="text"></input>
-                <button>Add</button>
-            </form>
-
-            <ul>
-                <li>{textToDo}</li>
-            </ul>
+            <Form onAddItemForm={onAddItemToDo}></Form>
+            <List itemsFromSetItems={items}></List>
         </div>
     );
 }
